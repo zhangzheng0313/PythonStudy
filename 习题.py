60a0786bb9768c42365d7a07847d4858e157ba53
zhangzheng0313 <462620811@qq.com>
2019-05-20 21:27:24 +0800

    by Zhangzheng

---
+++ 习题.py
@@ -0,0 +1,84 @@
+'''
+1.编写程序，要求用户输入1-100之间的整数并且判断，输入符合要求打印“正确”，否则打印“错误”
+temp = input("请输入一个1-100之间的整数：")
+if temp.isdigit():
+    temp = int(temp)
+    if 1 <= temp <= 100:
+        print("正确")
+    else:
+        print("错误")
+else:
+    print("错误")
+'''
+
+'''
+2.判定给定年份是否为闰年
+year = input("请输入年份：")
+if year.isdigit():
+    year = int(year)
+    if year % 4 == 0:
+        print(str(year) + "是闰年")
+    else:
+        print(str(year) + "不是闰年")
+else:
+    print("让你输入年份！")
+'''
+
+
+'''
+3.给用户3次机会，猜想程序生成的一个数字A，每次用户猜想后会提示数字是否正确以及用户输入的数字是大于A还是小鱼A，当机会用尽后会提示用户输掉游戏
+import random
+A = random.randint(1,100)
+times = 3
+while times:
+    num = input("请输入数字：")
+    if num.isdigit():
+        tmp = int(num)
+        if tmp == A:
+            print("你猜对了！")
+            break
+        elif tmp < A:
+            print("你输入的数字小了")
+ #           times = times - 1
+        else:
+            print("你输入的数字大了")
+ #           times = times - 1
+    else:
+        print("让你输入数字呢")
+#print("你的机会用完了")
+'''
+
+
+'''
+4.打印0-100所有奇数
+ls = range(0,101)
+for i in ls:
+    print(i)
+'''
+
+
+'''
+5.爱因斯坦阶梯
+x = 0
+while x < 1000:
+    if (x % 2 == 1) and (x % 3 == 2) and (x % 5 == 4) and (x % 6 == 5) and (x % 7 == 0):
+        print(x)
+        break
+    else:
+        x += 1
+print("循环结束")
+'''
+
+
+'''
+6.x=1,y=2,z=3,如何快速交换他们的值
+x = 1
+y = 2
+z = 3
+x, y ,z = z, x, y
+print(x)
+print(y)
+print(z)
+'''
+
+
